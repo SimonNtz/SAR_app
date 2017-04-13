e62ed3ef2ecd4d50297f2342f21433efa0126a87
Simon <simon.noetzlin@epfl.ch>
2017-04-13 11:16:19 +0200

    proc

--- SAR_test_proc.py
+++ SAR_test_proc.py
@@ -1,186 +1,150 @@
-#!/usr/bin/python
-###################################################################
-#
-#  Alaska Satellite Facility DAAC
-#
-#  Sentinel Radiometric Terrain Correction using Sentinel Toolbox (SNAP)
-#
-###################################################################
-#
-# Copyright (C) 2016 Alaska Satellite Facility
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
-###################################################################
-#
-#  SNAPPY RTC script
-#
-# TO USE:
-#    (1.) Install SNAP, Snappy, and S1Toolbox from ESA (http://step.esa.int/main/download/)
-#    (2.) search file for the word <FIX> and replace with appropriate path on your system
-#    (3.) Download the sample granules from ASF (https://vertex.daac.asf.alaska.edu/) by searching for these granule names:
-#            S1A_S1_SLC__1SSV_20141018T142338_20141018T142403_002885_003443_6545
-#            S1A_IW_GRDH_1SDV_20150316T235525_20150316T235550_005064_0065C2_6527
-#    (O.) optionally search for <OPTIONAL> and do what's required
-#
-###################################################################
 
-import os, sys
-sys.path.append('/home/snappy')
-sys.path.append('/opt/snap/')
-sys.path.append('/home/s1tbx')
-
+import os,sys
+sys.path.append('/home/snap-engine/snap-python/src/main/resources/snappy')
 import snappy
-from snappy import jpy
 from snappy import GPF
-from snappy import ProductIO
-from snappy import String
-from snappy import Product
-from snappy import ProductData
-from snappy import ProductIO
-from snappy import ProductUtils
-from snappy import VirtualBand
 from snappy import HashMap
+jpy = snappy.jpy
+import matplotlib
+matplotlib.use('GTK')
 import numpy as np
 import matplotlib.pyplot as plt
-from Queue import *
-from thread import *
-from time import *
+import scipy
+from scipy import ndimage
 
-iDebug = True
-iWriteEachStep = True
-file1=""
-filename=""
-targetCalibrated=""
-targetRTCd=""
+#s1paths = ( "S1A_IW_GRDH_1SDV_20151226T182813_20151226T182838_009217_00D48F_5D5F", "S1A_IW_GRDH_1SDV_20160424T182813_20160424T182838_010967_010769_AA98", "S1A_IW_GRDH_1SDV_20160518T182817_20160518T182842_011317_011291_936E", "S1A_IW_GRDH_1SDV_20160611T182819_20160611T182844_011667_011DC0_391B", "S1A_IW_GRDH_1SDV_20160705T182820_20160705T182845_012017_0128E1_D4EE", "S1A_IW_GRDH_1SDV_20160729T182822_20160729T182847_012367_013456_E8BF", "S1A_IW_GRDH_1SDV_20160822T182823_20160822T182848_012717_013FFE_90AF", "S1A_IW_GRDH_1SDV_20160915T182824_20160915T182849_013067_014B77_1FCD" )
+
+#todo veryfy with zipped files
+s1paths = [sys.argv[1]]
+s1meta = "manifest.safe"
+
+products = []
+print(s1paths)
+
+for s1path in s1paths:
+
+    s1prd= "/home/data/%s.SAFE/%s" % (s1path, s1meta)
+    reader = ProductIO.getProductReader("SENTINEL-1")
+    product = reader.readProductNodes(s1prd, None)
+    products.append(product)
+
+
+# Extract information about the Sentinel-1 GRD products:
+
+for product in products:
+
+    width = product.getSceneRasterWidth()
+    height = product.getSceneRasterHeight()
+    name = product.getName()
+    band_names = product.getBandNames()
+    print("Product: %s, %d x %d pixels" % (name, width, height))
+    print("Bands:   %s" % (list(band_names)))
+
+
+
+WKTReader = snappy.jpy.get_type('com.vividsolutions.jts.io.WKTReader')
+
+geom = WKTReader().read('POLYGON((-4.51 14.69,-4.477 14.227,-4.076 14.243,-4.054 14.642,-4.51 14.69))');
+
+HashMap = jpy.get_type('java.util.HashMap')
 GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
 
-q = Queue()
+parameters = HashMap()
+parameters.put('copyMetadata', True)
+parameters.put('geoRegion', geom)
+
+subsets = []
+
+for product in products:
+
+    subset = GPF.createProduct('Subset', parameters, product)
+    subsets.append(subset)
+
+# Step 1: Pre-processing - Calibration
+
+parameters = HashMap()
+
+parameters.put('auxFile', 'Latest Auxiliary File')
+parameters.put('outputSigmaBand', True)
+parameters.put('selectedPolarisations', 'VV')
+
+calibrates = []
+
+for subset in subsets:
+
+    calibrate = GPF.createProduct('Calibration', parameters, subset)
+    calibrates.append(calibrate)
+
+# Step 2: Pre-processing - Speckle filtering
+
+parameters = HashMap()
+
+parameters.put('filter', 'Lee')
+parameters.put('filterSizeX', 7)
+parameters.put('filterSizeY', 7)
+parameters.put('dampingFactor', 2)
+parameters.put('edgeThreshold', 5000.0)
+parameters.put('estimateENL', True)
+parameters.put('enl', 1.0)
+
+speckles = []
+
+                                                                                                96,0-1        31%
+for calibrate in calibrates:
+
+    speckle = GPF.createProduct('Speckle-Filter', parameters, calibrate)
+    speckles.append(speckle)
+
+parrameters = HashMap()
+
+parameters.put('demResamplingMethod', 'NEAREST_NEIGHBOUR')
+parameters.put('imgResamplingMethod', 'NEAREST_NEIGHBOUR')
+parameters.put('demName', 'SRTM 3Sec')
+parameters.put('pixelSpacingInMeter', 10.0)
+parameters.put('sourceBands', 'Sigma0_VV')
+
+terrains = []
 
 
-def DestinationThread() :
-  while True :
-    f, args = q.get()
-    f(*args)
+for speckle in speckles :
+
+    terrain = GPF.createProduct('Terrain-Correction', parameters, speckle)
+    terrains.append(terrain)
+
+parameters = HashMap()
+
+lineartodbs= []
+
+for terrain in terrains:
+
+    lineartodb = GPF.createProduct('linearToFromdB', parameters, terrain)
+    lineartodbs.append(lineartodb)
+
+def rot_crop(c, ang):
+    rot_c = ndimage.rotate(c, ang)
+    lx, ly = rot_c.shape
+    crop_rot =  rot_c[lx/3:-lx/3, ly/4:-ly/4]
+   # rotate_lena_noreshape = ndimage.rotate(c, ang, reshape=False)
+    return(rot_c)
 
 
-def readFiles(filename1):
-    global file1
-    global filename
+def printBand(product, band, vmin, vmax):
 
-    if iDebug: print filename1
-    file1 = ProductIO.readProduct(filename1)
-    filename = file1.getName()
-    if iDebug: print "Read file: " + file1.getName()
+    band = product.getBand(band)
+    w = band.getRasterWidth()
+    h = band.getRasterHeight()
 
+    band_data = np.zeros(w * h, np.float32)
+    band.readPixels(0, 0, w, h, band_data)
 
-def writeFiles(target, strStep):
-    global filename
-    # <FIX>:  fix this path
-    ofile = '/home/' + filename + strStep
-    if target:
-        ProductIO.writeProduct(target, ofile, 'GeoTIFF-BigTIFF')   # <Optional> change to other types here: BEAM-DIMAP, GeoTIFF-BigTIFF, etc.
+    band_data.shape = h, w
+    imgplot = plt.imshow(rot_crop(band_data, -10.75), cmap=plt.cm.binary_r, vmin=vmin, vmax=vmax)
+    plt.axis('off')
+    plt.savefig(name + '.png')#,bbox_inches='tight',frameon=False, transparent=True,pad_inches=0)
+    plt.show()
+    print('Printed!')
 
+for lineartodb in lineartodbs :
+    imgplot = printBand(lineartodb, 'Sigma0_VV_db', -25, 5)
 
-def calibration():
-	### Calibration
-	global targetCalibrated
-	global file1
-
-
-	parameters = HashMap()
-	if file1 == "":
-		print "Error - File 1 missing"
-		return
-
-	targetCalibrated = GPF.createProduct("Calibration", parameters, file1)
-	if iDebug: print "Calibration working"
-
-
-
-def terrainCorrection():
-    ###  Terrain-Correction
-    global targetCalibrated
-    global targetRTCd
-
-    if targetCalibrated=="":
-    	print "targetCalibrated missing"
-    	return
-
-    parameters = HashMap()
-    # OPTIONAL: parameters.put('Search Window Accuracy in Azimuth Direction', 2)
-    targetRTCd = GPF.createProduct("Terrain-Correction", parameters, targetCalibrated)
-    if iDebug: print "TerrainCorrection working"
-
-
-
-
-### Main ###
-#
-#
-# <FIX>: download the sample file /or/ change this to your granule
-# <OPTIONAL>:  fix this path
-# SLC: filename1 = "/S1A_S1_SLC__1SSV_20141018T142338_20141018T142403_002885_003443_6545.zip"
-# GRD: filename1 = "/S1A_IW_GRDH_1SDV_20150316T235525_20150316T235550_005064_0065C2_6527.zip"
-filename1 = "S1A_IW_GRDH_1SDV_20150316T235525_20150316T235550_005064_0065C2_6527.zip"
-
-start_new_thread( DestinationThread, tuple() )
-
-print "Snappy RTC: start"
-sleep( 1 )
-q.put( (readFiles, [filename1]) )
-sleep( 1 )
-
-### Calibration step
-q.put( (calibration, "") )
-sleep( 1 )
-if targetCalibrated and iWriteEachStep:
-    q.put( (writeFiles, [targetCalibrated, "_Cal"]) )
-    sleep( 1 )
-
-if (targetCalibrated) and iDebug:
-    print "Calibration completed"
-elif not(targetCalibrated) and iDebug:
-    print "Calibration failed to complete!"
-
-### Terrain Correction step
-q.put( (terrainCorrection, "") )
-sleep( 1 )
-if targetRTCd:
-    q.put( (writeFiles, [targetRTCd, "_TC"]) )
-    sleep( 1 )
-
-if (targetRTCd) and iDebug:
-    print "TerrainCorrection completed"
-elif not(targetRTCd) and iDebug:
-    print "TerrainCorrection failed to complete!"
-
-print "Snappy RTC: end"
-
-
-### CLeanup temporary image files
-if iDebug: print "Cleanup tmp files"
-myStr = "image"
-path = "/home/.snap/var/cache/temp"
-dirs = os.listdir( path )
-
-for file in dirs:
-    if iDebug: print file
-    # make sure it's an image file
-    if file.startswith(myStr):
-        os.remove(path + "/" + file)
-if iDebug: print "Cleanup complete"
-
-###  Done
-print "Thank you for using ASF's Snappy RTC script!"
+                                                                                                    198,0-1       Bot

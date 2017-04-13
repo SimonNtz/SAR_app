9de6ea5e926518a49164b01b206dedee3e308c01
Simon <simon.noetzlin@epfl.ch>
2017-04-13 15:37:34 +0200

    pas le time

--- SAR_test_proc.py
+++ SAR_test_proc.py
@@ -11,7 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 from scipy import ndimage
-
+import gc
 #s1paths = ( "S1A_IW_GRDH_1SDV_20151226T182813_20151226T182838_009217_00D48F_5D5F", "S1A_IW_GRDH_1SDV_20160424T182813_20160424T182838_010967_010769_AA98", "S1A_IW_GRDH_1SDV_20160518T182817_20160518T182842_011317_011291_936E", "S1A_IW_GRDH_1SDV_20160611T182819_20160611T182844_011667_011DC0_391B", "S1A_IW_GRDH_1SDV_20160705T182820_20160705T182845_012017_0128E1_D4EE", "S1A_IW_GRDH_1SDV_20160729T182822_20160729T182847_012367_013456_E8BF", "S1A_IW_GRDH_1SDV_20160822T182823_20160822T182848_012717_013FFE_90AF", "S1A_IW_GRDH_1SDV_20160915T182824_20160915T182849_013067_014B77_1FCD" )
 
 #TODO check with zipped files
@@ -122,8 +122,8 @@ for terrain in terrains:
 def rot_crop(c, ang):
     rot_c = ndimage.rotate(c, ang)
     lx, ly = rot_c.shape
-    crop_rot =  rot_c[lx/3:-lx/3, ly/4:-ly/4]
-    return(rot_c)
+    crop_rot =  rot_c[lx/6:-lx/6, ly/6:-ly/6]
+    return(crop_rot)
 
 
 #TODO Try to use ImageIO instead of pyplot
@@ -149,8 +149,8 @@ def printBand(product, band, vmin, vmax):
 
 for lineartodb in lineartodbs :
     printBand(lineartodb, 'Sigma0_VV_db', -25, 5)
-     plt.close()
-    del calibrates, terrrains, speckles, lineartodbs
+    plt.close()
+    #del calibrates, terrrains, speckles, lineartodbs
     gc.collect()
 
                                                                                                     198,0-1       Bot

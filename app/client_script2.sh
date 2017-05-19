9d6a3796a19d48ea898e8f9cf293fdac1c93f509
Simon <simon.noetzlin@epfl.ch>
2017-05-19 14:17:49 +0200

    last client runnner script

--- app/client_script2.sh
+++ app/client_script2.sh
@@ -2,8 +2,14 @@
 
 #TODO verify SS-client install
 
+#Recover token in cookies-nuvla.txt
+#slipstream login -u simon1992 -p 12mc0v2ee64o9
+
+NUVLA_TOKEN=`cat ~/.slipstream/cookies-nuvla.txt | grep -v \#`
+
+
 INPUT_SIZE=`cat product_list.cfg | wc -l`
 INPUT_LIST=`cat product_list.cfg`
 
-ss-execute --parameters="mapper:multiplicity=$INPUT_SIZE","mapper:product_url='$INPUT_LIST'","mapper:cloudservice=eo-cesnet-cz1","reducer:cloudservice=eo-cesnet-cz1" --keep-running="always" EO_Sentinel_1/procSAR
+ss-execute --parameters="mapper:multiplicity=$INPUT_SIZE","mapper:product_url='$INPUT_LIST'","mapper:cloudservice=eo-cesnet-cz1","reducer:cloudservice=eo-cesnet-cz1","reducer:nuvla_token='a $equal b'" --keep-running="always" EO_Sentinel_1/procSAR
 #ss-execute --parameters="mapper:multiplicity=3","mapper:product_url='$INPUT_LIST'","mapper:cloudservice=eo-cesnet-cz1","reducer:cloudservice=eo-cesnet-cz1" EO_Sentinel_1/procSAR

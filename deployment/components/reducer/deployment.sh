b16fb00b5012a112d9be5cb6728e9e8de4577a75
Simon <simon.noetzlin@epfl.ch>
2017-05-22 11:04:07 +0200

    compents' recipe refactoring

--- deployment/components/reducer/deployment.sh
+++ deployment/components/reducer/deployment.sh
@@ -1,49 +1,46 @@
 #!/bin/bash
-#s3cmd --> Move to built image
-set -ex
 cd /home
 
+# S3_CFG=~/.s3cfg
+# S3_BUCKET=s3://eodata_output
 
-S3_CFG=~/.s3cfg
-S3_BUCKET=s3://eodata_output
+# set_s3() {
+#
+#     cat > $S3_CFG <<EOF
+#
+#     [default]
+#     host_base = sos.exo.io
+#     host_bucket = %(bucket)s.sos.exo.io
+#
+#     access_key = $$
+#     secret_key = $$
+#     use_https = True
+#     signature_v2 = True
+#
+# EOF
+#
+# (printf '\n\n\n\n\n\n\n\ny') | s3cmd --configure
+#
+# }
 
-set_s3() {
-
-    cat > $S3_CFG <<EOF
-
-    [default]
-    host_base = sos.exo.io
-    host_bucket = %(bucket)s.sos.exo.io
-
-    access_key = $$
-    secret_key = $$
-    use_https = True
-    signature_v2 = True
-
-EOF
-
-(printf '\n\n\n\n\n\n\n\ny') | s3cmd --configure
-
-}
-
-install_slipstream_api(){
-    pip install https://github.com/slipstream/SlipStreamPythonAPI/archive/master.zip
-    mv /usr/local/lib/python2.7/dist-packages/slipstream/api /opt/slipstream/client/lib/slipstream/
-    rm -Rf /usr/local/lib/python2.7/dist-packages/slipstream
-    ln -s /opt/slipstream/client/lib/slipstream /usr/local/lib/python2.7/dist-packages/slipstream
-}
-
-
-create_cookie(){
-    cat >cookies-nuvla.txt<<EOF
-# Netscape HTTP Cookie File
-# http://curl.haxx.se/rfc/cookie_spec.html
-# This is a generated file!  Do not edit.
-
-"$@"
-#nuv.la	FALSE	/	TRUE	1495616337	com.sixsq.slipstream.cookie	token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNpbW9uMTk5MiIsInJvbGVzIjoiVVNFUiBBTk9OIiwiZXhwIjoxNDk1NjE2MzM3fQ.SRdnhjIJuRu66MXKSUkUrwIh8_NGggG2plhg9RwxuPt2PZv1BMKmeBYPDuFE9gCl5sVMImDA4HLV8X5e2LSAbfkFIhBcm9B_7kCu0x9ZkwAo7mmeurC6JBwUg3n4PMTYnX-Cz_UeSgxYjbT-C7RhGQT0cKog9ZOL538vdktuG6WuLUEp8IpyrVKKc5yOTvXmK71s0tO1hhf-IEq7hd31CHmO__1iRA1wcxt1Bl2Kn4rkSNb_JOBfyQw__lv-Y3gGk2YOev5ly5rX5JySIUCGtKCfmPmrj4zIV5_UYGFl_o2PdmMOIRNK0GIR7wlpTN0uIyawKabr2YcwRvCA8OFOdA
-EOF
-}
+# install_slipstream_api(){
+#     pip install https://github.com/slipstream/SlipStreamPythonAPI/archive/master.zip
+#     mv /usr/local/lib/python2.7/dist-packages/slipstream/api /opt/slipstream/client/lib/slipstream/
+#     rm -Rf /usr/local/lib/python2.7/dist-packages/slipstream
+#     ln -s /opt/slipstream/client/lib/slipstream /usr/local/lib/python2.7/dist-packages/slipstream
+# }
+#
+#
+# create_cookie(){
+#     cat >cookies-nuvla.txt<<EOF
+# # Netscape HTTP Cookie File
+# # http://curl.haxx.se/rfc/cookie_spec.html
+# # This is a generated file!  Do not edit.
+#
+# "$@"
+# #nuv.la	FALSE	/	TRUE	1495616337	com.sixsq.slipstream.cookie	token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNpbW9uMTk5MiIsInJvbGVzIjoiVVNFUiBBTk9OIiwiZXhwIjoxNDk1NjE2MzM3fQ.SRdnhjIJuRu66MXKSUkUrwIh8_NGggG2plhg9RwxuPt2PZv1BMKmeBYPDuFE9gCl5sVMImDA4HLV8X5e2LSAbfkFIhBcm9B_7kCu0x9ZkwAo7mmeurC6JBwUg3n4PMTYnX-Cz_UeSgxYjbT-C7RhGQT0cKog9ZOL538vdktuG6WuLUEp8IpyrVKKc5yOTvXmK71s0tO1hhf-IEq7hd31CHmO__1iRA1wcxt1Bl2Kn4rkSNb_JOBfyQw__lv-Y3gGk2YOev5ly5rX5JySIUCGtKCfmPmrj4zIV5_UYGFl_o2PdmMOIRNK0GIR7wlpTN0uIyawKabr2YcwRvCA8OFOdA
+# EOF
+# }
 
 post_event() {
     cat >pyScript.py<<EOF
@@ -85,13 +82,18 @@ echo -n  $@ | xargs -d ' ' -I% bash -c '(nc -l 808%  0<&- 1>%.png) &'
 }
 
 
-# Launch a timeout on the ready state for all mapper of '$ids
+# Run multiple daemons depending on the mapper VM multiplicity and
+# whithin these a timeout checking mapper's ready state is triggered.
+
 check_ready() {
     echo -n $ids | xargs -d ' ' -I% bash -c '(
     ss-get --timeout 1800 mapper.%:ready
     echo 'mapper.'%':ready' >>readylock.md
     exit 0) &'
 }
+
+# The number of line existing in "readylock.md" file indicates
+# how many mappers are is ready state.
 
 count_ready() {
  echo `cat readylock.md | wc -l`
@@ -110,21 +112,23 @@ if [ -z "$ids" ]; then
 else
    set_listeners $ids
    check_ready
-   create_cookie "`ss-get nuvla_token`"
-   install_slipstream_api
-   cat cookies-nuvla.txt
-   # Wait before all mappers are in ready state
-   while [ $(count_ready) -ne `ss-get mapper:multiplicity` ]; do
-   sleep 100; done
+  #  create_cookie "`ss-get nuvla_token`"
+  #  install_slipstream_api
+  #  cat cookies-nuvla.txt
+
+  # Wait before all mappers are in ready state i.e. equals to mappers' multiplicity integer
+   while [ $(count_ready) -ne `ss-get mapper:multiplicity` ]; do sleep 100; done
+
    post_event "Reducer has finished to download corrected product"
+  # Create the final output
    ls -l *.png
    convert -delay 20 -loop 0 *.png $output
 fi
 
-ss-display "$(date)"
-set_s3
+#set_s3
 
-s3cmd put $output s3://eodata_output
+# Push animated GIF to the object store through S3
+s3cmd put $output $S3_BUCKET
 ss-set ss:url.service https://sos.exo.io/eodata_output/$output
 post_event 'Output file available on object store'
 ss-set ready true
--- deployment/components/reducer/post_install.sh
+++ deployment/components/reducer/post_install.sh
@@ -1,3 +1,45 @@
-#!/bin/bash
+#!env/bin bash
+
+set_s3() {
+    S3_CFG=~/.s3cfg
+    cat > $S3_CFG <<EOF
+
+    [default]
+    host_base = sos.exo.io
+    host_bucket = %(bucket)s.sos.exo.io
+
+    access_key = $$
+    secret_key = $$
+    use_https = True
+    signature_v2 = True
+
+EOF
+
+(printf '\n\n\n\n\n\n\n\ny') | s3cmd --configure
+
+}
+
+install_slipstream_api(){
+    pip install https://github.com/slipstream/SlipStreamPythonAPI/archive/master.zip
+    mv /usr/local/lib/python2.7/dist-packages/slipstream/api /opt/slipstream/client/lib/slipstream/
+    rm -Rf /usr/local/lib/python2.7/dist-packages/slipstream
+    ln -s /opt/slipstream/client/lib/slipstream /usr/local/lib/python2.7/dist-packages/slipstream
+}
+
+
+create_cookie(){
+    cat >cookies-nuvla.txt<<EOF
+# Netscape HTTP Cookie File
+# http://curl.haxx.se/rfc/cookie_spec.html
+# This is a generated file!  Do not edit.
+
+"$@"
+#nuv.la	FALSE	/	TRUE	1495616337	com.sixsq.slipstream.cookie	token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNpbW9uMTk5MiIsInJvbGVzIjoiVVNFUiBBTk9OIiwiZXhwIjoxNDk1NjE2MzM3fQ.SRdnhjIJuRu66MXKSUkUrwIh8_NGggG2plhg9RwxuPt2PZv1BMKmeBYPDuFE9gCl5sVMImDA4HLV8X5e2LSAbfkFIhBcm9B_7kCu0x9ZkwAo7mmeurC6JBwUg3n4PMTYnX-Cz_UeSgxYjbT-C7RhGQT0cKog9ZOL538vdktuG6WuLUEp8IpyrVKKc5yOTvXmK71s0tO1hhf-IEq7hd31CHmO__1iRA1wcxt1Bl2Kn4rkSNb_JOBfyQw__lv-Y3gGk2YOev5ly5rX5JySIUCGtKCfmPmrj4zIV5_UYGFl_o2PdmMOIRNK0GIR7wlpTN0uIyawKabr2YcwRvCA8OFOdA
+EOF
+}
+
+set_s3
+install_slipstream_api
+create_cookie
 
 sudo rm -rf /var/lib/cloud/instance/sem/*

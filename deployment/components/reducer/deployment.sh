a8111821fc4b993c241de5f617e8fd588542c399
Simon <simon.noetzlin@epfl.ch>
2017-05-29 17:30:44 +0200

    testing

--- deployment/components/reducer/deployment.sh
+++ deployment/components/reducer/deployment.sh
@@ -26,63 +26,63 @@ EOF
 
 }
 
-install_slipstream_api(){
-    pip install https://github.com/slipstream/SlipStreamPythonAPI/archive/master.zip
-    mv /usr/local/lib/python2.7/dist-packages/slipstream/api /opt/slipstream/client/lib/slipstream/
-    rm -Rf /usr/local/lib/python2.7/dist-packages/slipstream
-    ln -s /opt/slipstream/client/lib/slipstream /usr/local/lib/python2.7/dist-packages/slipstream
-}
+# install_slipstream_api(){
+#     pip install https://github.com/slipstream/SlipStreamPythonAPI/archive/master.zip
+#     mv /usr/local/lib/python2.7/dist-packages/slipstream/api /opt/slipstream/client/lib/slipstream/
+#     rm -Rf /usr/local/lib/python2.7/dist-packages/slipstream
+#     ln -s /opt/slipstream/client/lib/slipstream /usr/local/lib/python2.7/dist-packages/slipstream
+# }
 
-cookiefile=/home/cookies-nuvla.txt
+# cookiefile=/home/cookies-nuvla.txt
 
-create_cookie(){
-#  [ -z "$@" ] || return
-    cat >cookies-nuvla.txt<<EOF
-# Netscape HTTP Cookie File
+# create_cookie(){
+# #  [ -z "$@" ] || return
+#     cat >cookies-nuvla.txt<<EOF
+# # Netscape HTTP Cookie File
+#
+# "$1"
+# EOF
+# }
+#
+# post_event() {
+#   [ -f $cookiefile ] || return
+#   username=$(get_username)
+#   duiid=$(get_DUIID)
+#   cat >pyScript.py<<EOF
+# import sys
+# from slipstream.api import Api
+# api = Api(cookie_file='$cookiefile')
+# log = str(sys.argv[1]).translate(None, "[]")
+# print log
+# event = {'acl': {u'owner': {u'principal': u'$username'.strip(), u'type': u'USER'},
+#         u'rules': [{u'principal': u'$username'.strip(),
+#         u'right': u'ALL',
+#         u'type': u'USER'},
+#         {u'principal': u'ADMIN',
+#         u'right': u'ALL',
+#         u'type': u'ROLE'}]},
+#   'content': {u'resource': {u'href': u'run/'+ u'$get_DUIID'.strip()},
+#                                         u'state': log},
+#   'severity': u'low',
+#   'timestamp': '$(get_timestamp)',
+#   'type': u'state'}
+#
+# api.cimi_add('events', event)
+# EOF
+# python pyScript.py "$@"
+# }
 
-"$1"
-EOF
-}
-
-post_event() {
-  [ -f $cookiefile ] || return
-  username=$(get_username)
-  duiid=$(get_DUIID)
-  cat >pyScript.py<<EOF
-import sys
-from slipstream.api import Api
-api = Api(cookie_file='$cookiefile')
-log = str(sys.argv[1]).translate(None, "[]")
-print log
-event = {'acl': {u'owner': {u'principal': u'$username'.strip(), u'type': u'USER'},
-        u'rules': [{u'principal': u'$username'.strip(),
-        u'right': u'ALL',
-        u'type': u'USER'},
-        {u'principal': u'ADMIN',
-        u'right': u'ALL',
-        u'type': u'ROLE'}]},
-  'content': {u'resource': {u'href': u'run/'+ u'$get_DUIID'.strip()},
-                                        u'state': log},
-  'severity': u'low',
-  'timestamp': '$(get_timestamp)',
-  'type': u'state'}
-
-api.cimi_add('events', event)
-EOF
-python pyScript.py "$@"
-}
-
-get_DUIID() {
-    awk -F= '/diid/ {print $2}' /opt/slipstream/client/sbin/slipstream.context
-}
-
-get_timestamp() {
-    echo `date --utc +%FT%T.%3NZ`
-}
-
-get_username() {
-  awk -F= '/username/ {print $2}' /opt/slipstream/client/sbin/slipstream.context
-}
+# get_DUIID() {
+#     awk -F= '/diid/ {print $2}' /opt/slipstream/client/sbin/slipstream.context
+# }
+#
+# get_timestamp() {
+#     echo `date --utc +%FT%T.%3NZ`
+# }
+#
+# get_username() {
+#   awk -F= '/username/ {print $2}' /opt/slipstream/client/sbin/slipstream.context
+# }
 
 # Lauch netcat daemons for each  product
 set_listeners() {

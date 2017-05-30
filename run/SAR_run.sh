3781e85ed84a3e323542fc4a846bdc6c971bb9fa
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2017-05-30 09:12:23 +0200

    updated doc and run script

--- run/SAR_run.sh
+++ run/SAR_run.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
-# *** CLIENT SCRIPT ***
-#
+
 # - Bash script launching the the SlipStream application
 # - its unique parameter is the cloud service selected by the client
 # - it retrieves the input list from file "product_list.cfg"
-#
 
-#Recover token in cookies-nuvla.txt
-slipstream login -u $SLIPSTREAM_USERNAME -p $SLIPSTREAM_PASSWORD
-
-NUVLA_TOKEN=`cat ~/.slipstream/cookies-nuvla.txt | grep -v \#`
-
+# Connector instance name as defined on https://nuv.la for which user has
+# provided credentials in its profile.
 CLOUD="$1"
-#CLOUD='eo-cesnet-cz1'
-#CLOUD='ec2-eu-west'
 
-INPUT_SIZE=`cat product_list.cfg | sed '/^\s*#/d;/^\s*$/d' | wc -l`
-INPUT_LIST=`cat product_list.cfg | sed '/^\s*#/d;/^\s*$/d'`
+INPUT_SIZE=`awk '/^[a-zA-z]/' product_list.cfg | wc -l`
+INPUT_LIST=`awk '/^[a-zA-z]/' product_list.cfg`
 
 ss-execute \
-    --keep-running="always" \
+    --keep-running="never" \
     --parameters="
     mapper:multiplicity=$INPUT_SIZE,
-    mapper:product_url=$INPUT_LIST,
+    mapper:product-list=$INPUT_LIST,
     mapper:cloudservice=$CLOUD,
-    reducer:cloudservice=$CLOUD,
-    reducer:nuvla_token=$NUVLA_TOKEN" \
+    reducer:cloudservice=$CLOUD" \
     EO_Sentinel_1/procSAR

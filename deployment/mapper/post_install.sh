2065e127cef59dfab5dd1a9053640713c86a6d78
Simon <simon.noetzlin@epfl.ch>
2017-06-01 09:29:10 +0200

    try to build without snappy

--- deployment/mapper/post_install.sh
+++ deployment/mapper/post_install.sh
@@ -65,5 +65,6 @@ configure_python_interface() {
 }
 
 install_S1_toolbox
-configure_python_interface || configure_python_interface
+ls /opt/
+#configure_python_interface || configure_python_interface
 #install_slipstream_api

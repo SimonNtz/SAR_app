370a318e0bbb2016f6f03db7d3dcd5b0e2df2d52
Simon <simon.noetzlin@epfl.ch>
2017-06-01 15:37:56 +0200

    retry with && condition

--- deployment/mapper/post_install.sh
+++ deployment/mapper/post_install.sh
@@ -57,8 +57,9 @@ install_S1_toolbox() {
 configure_python_interface() {
     #TODO: check if SNAP is correctly installed
     apt install -y strace
-    sudo bash /opt/snap/bin/snap --nogui --nosplash --python /usr/bin/python2.7 \
-        /home/snap-engine/snap-python/src/main/resources/snappy 
+    bash /opt/snap/bin/snap --nogui --nosplash --python /usr/bin/python2.7 \
+        /home/snap-engine/snap-python/src/main/resources/snappy && bash /opt/snap/bin/snap --nogui --nosplash --python /usr/bin/python2.7 \
+            /home/snap-engine/snap-python/src/main/resources/snappy
     # VERIF SNAPPY INTERFACE
     cd /opt/snap/snap/modules/lib/x86_64/
     ln -s ../amd64/libjhdf.so

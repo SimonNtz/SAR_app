0259d238b37c03cf406379bc89428a45e8e83d9e
Simon <simon.noetzlin@epfl.ch>
2017-06-07 15:41:56 +0200

    m

--- deployment/mapper/post_install.sh
+++ deployment/mapper/post_install.sh
@@ -70,13 +70,13 @@ Xvfb :1 -screen 0 1024x768x16 &
 }
 
   configure_python_interface() {
-      Xvfb :1 -screen 0 1024x768x16 &
-      XPID=$!
-      export DISPLAY=:1
+      #Xvfb :1 -screen 0 1024x768x16 &
+      #XPID=$!
+      #export DISPLAY=:1
       #TODO: check if SNAP is correctly installed
       cd /opt/snap/bin
-      ./snap --nosplash --python /usr/bin/python2.7 &
-      kill -15 $XPID
+      ./snap --nosplash --python /usr/bin/python2.7 #&
+      #kill -15 $XPID
       cd /opt/snap/snap/modules/lib/x86_64/
       ln -s ../amd64/libjhdf.so
       ln -s ../amd64/libjhdf5.so

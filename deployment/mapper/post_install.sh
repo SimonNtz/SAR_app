26c301cf5cd4427c9f75bc295fc90ad27ace8b91
Simon <simon.noetzlin@epfl.ch>
2017-06-08 16:51:24 +0200

    tel me

--- deployment/mapper/post_install.sh
+++ deployment/mapper/post_install.sh
@@ -29,12 +29,12 @@ install_S1_toolbox() {
         $SNAP_LOC=/usr/local/snap
       fi
 
-      ./$SNAP_LOC --nosplash --python /usr/bin/python2.7 &
-      #sleep 5
+      snap --nosplash --python /usr/bin/python2.7 &
+
       kill -15 $XPID
-      cd $SNAP_LOC/../snap/modules/lib/x86_64/
-      ln -s ../amd64/libjhdf.so
-      ln -s ../amd64/libjhdf5.so
+      cd $SNAP_LOC/snap/modules/lib/x86_64/
+      ln -s amd64/libjhdf.so
+      ln -s amd64/libjhdf5.so
   }
 
 install_S1_toolbox

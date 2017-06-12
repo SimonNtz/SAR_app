9d22f9cf264bd697bf06381fe65c267311380f4c
Simon <simon.noetzlin@epfl.ch>
2017-06-12 17:20:26 +0200

    a

--- deployment/mapper/post_install.sh
+++ deployment/mapper/post_install.sh
@@ -29,9 +29,9 @@ install_S1_toolbox() {
         SNAP_LOC=/usr/local/snap
       fi
 
-      snap --nosplash --python /usr/bin/python2.7 &
+      snap --nogui --nosplash --python /usr/bin/python2.7 &
 
-      sleep 240
+      wait $!
 
       kill -15 $XPID
 

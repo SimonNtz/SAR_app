7a86d5f14af74e804a1b22a8a01697995d114b73
Simon <simon.noetzlin@epfl.ch>
2017-06-15 13:25:46 +0200

    last change on reducer

--- deployment/reducer/deployment.sh
+++ deployment/reducer/deployment.sh
@@ -50,6 +50,6 @@ post_event "Reducer has finished to download corrected product."
 
 
 # Create the final output
-SAR_convert=SAR_convert`ss-get converter`.sh
+SAR_convert=SAR_convert_`ss-get converter`.sh
 cd ~/SAR_proc/reducer/
 ./$SAR_convert

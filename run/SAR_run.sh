3297bbe11ef6772d63aa23210ff5bfa9047748a9
Simon <simon.noetzlin@epfl.ch>
2017-06-21 09:13:25 +0200

    modif run script by adding repository

--- run/SAR_run.sh
+++ run/SAR_run.sh
@@ -3,12 +3,15 @@
 set -o pipefail
 
 # Bash script launching the the SlipStream application.
-# Its unique parameter is the cloud service selected by the client.
-# It retrieves the input list from file "product_list.cfg".
+# The parameters are the cloud service choosed by the client
+# and its Github repository (optional) following the SAR_proc model at
+# https://github.com/SimonNtz/SAR_proc.
+# Input data is stored in file list "product_list.cfg".
 
 # Connector instance name as defined on https://nuv.la for which user has
 # provided credentials in its profile.
 CLOUD="$1"
+GH_REPO="$2"
 
 INPUT_SIZE=`awk '/^[a-zA-z]/' product_list.cfg | wc -l`
 INPUT_LIST=`awk '/^[a-zA-z]/' product_list.cfg`
@@ -26,7 +29,9 @@ python -u `which ss-execute` \
     mapper:multiplicity=$INPUT_SIZE,
     mapper:product-list=$INPUT_LIST,
     mapper:cloudservice=$CLOUD,
-    reducer:cloudservice=$CLOUD" \
+    mapper:git-repo=$GH_REPO,
+    reducer:cloudservice=$CLOUD,
+    reducer:git-repo=$GH_REPO" \
     EO_Sentinel_1/procSAR 2>&1 | tee $LOG
 
 if [ "$?" == "0" ]; then

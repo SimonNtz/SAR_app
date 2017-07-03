6a7c8c2e9bf4beb853ccdd45478f03446d5d40d0
Simon <simon.noetzlin@epfl.ch>
2017-07-03 10:48:18 +0200

    snap script correction

--- deployment/mapper/post_install.sh
+++ deployment/mapper/post_install.sh
@@ -15,7 +15,7 @@ install_S1_toolbox() {
 
     curl -O http://step.esa.int/downloads/5.0/installers/$SNAP_INSTALLER
     chmod +x $SNAP_INSTALLER
-    echo -e "o\n1\n1n\n\n2\ny\ny\n/usr/bin/python2.7\ny\n" | ./$SNAP_INSTALLER
+    echo -e "o\n1\n\n2\ny\ny\n/usr/bin/python2.7\ny\n" | ./$SNAP_INSTALLER
 }
 
   configure_python_interface() {

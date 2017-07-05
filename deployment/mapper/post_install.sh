a7f954aefb9be1d073faefe9d94eb0521dafc0a5
Simon <simon.noetzlin@epfl.ch>
2017-07-05 09:44:06 +0200

    Debconf select non-interactive dialog

--- deployment/mapper/post_install.sh
+++ deployment/mapper/post_install.sh
@@ -9,32 +9,33 @@ set -o pipefail
 
 install_S1_toolbox() {
 
-    JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
-    export JAVA_HOME
-    PATH=$PATH:$JAVA_HOME/bin
-    SNAP_LOC=/usr/local/snap
-    SNAP_INSTALLER=esa-snap_sentinel_unix_5_0.sh
+  JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+  export JAVA_HOME
+  PATH=$PATH:$JAVA_HOME/bin
+  SNAP_LOC=/usr/local/snap
+  SNAP_INSTALLER=esa-snap_sentinel_unix_5_0.sh
 
-    # Impose the SNAP installation output directory
-    # b.c. the cloud service can change it deliberately
-    curl -O http://step.esa.int/downloads/5.0/installers/$SNAP_INSTALLER
-    chmod +x $SNAP_INSTALLER
-    echo -e "o\n1\n/usr/local/snap\n2\ny\ny\ny\n/usr/bin/python2.7\ny\n" | ./$SNAP_INSTALLER
-    #echo -e "o\n1\n\n\n2,3\ny\n\ny\n\ny\n"
+  # Impose the SNAP installation output directory
+  # b.c. the cloud service can change it deliberately
+  curl -O http://step.esa.int/downloads/5.0/installers/$SNAP_INSTALLER
+  chmod +x $SNAP_INSTALLER
+  echo -e "o\n1\n/usr/local/snap\n2\ny\ny\ny\n/usr/bin/python2.7\ny\n" | ./$SNAP_INSTALLER
 
-    # File system configuration for SNAP' datafiles
-    cd $SNAP_LOC/snap/modules/lib/x86_64/
-    ln -s ../amd64/libjhdf.so
-    ln -s ../amd64/libjhdf5.so
+  # File system configuration for SNAP' datafiles
+  cd $SNAP_LOC/snap/modules/lib/x86_64/
+  ln -s ../amd64/libjhdf.so
+  ln -s ../amd64/libjhdf5.so
 }
 
 install_filebeat() {
-apt-get update
-sudo apt-get install -y apt-transport-https
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
-apt-get update
-sudo apt-get install -y filebeat
+
+  apt-get update
+  sudo apt-get install -y apt-transport-https
+  wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+  echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+  apt-get update
+  echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+  sudo apt-get install -y filebeat
 }
 
 # configure_python_interface() {
